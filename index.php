535c70b3967f9df4be1cdaa763a142b0be908ecd
Ryan McCullagh <ryan@amezmo.com>
2019-05-29 00:20:42 -0500

    update

--- index.php
+++ index.php
@@ -4,15 +4,13 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-try {
-    $dbo = new PDO("mysql:host=localhost", "ryan", "password");
-    $dbo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $dbo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-} catch (PDOException $e) {
-    var_dump($e);
-}
+$user = 'ryan';
+$pass = 'password';
+$server = 'localhost';
 
-$rs = $dbo->query("SHOW DATABASES");
-while ($h = $rs->fetch(PDO::FETCH_NUM)) {
-    echo $r[0] . "<br>";
+$dbh = new PDO("mysql:host=$server", $user, $pass);
+$dbs = $dbh->query('SHOW DATABASES');
+
+while (($db = $dbs->fetchColumn(0)) !== false) {
+    echo $db . '<br>';
 }
