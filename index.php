56e682eb947f7b38b79023a168bffd934c2c9171
Ryan McCullagh <ryan@amezmo.com>
2019-05-29 00:15:06 -0500

    use index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+try {
+    $DBH = new PDO("mysql:host=localhost", "ryan", "password");
+    $DBH->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $DBH->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+} catch (PDOException $e) {
+    var_dump($e);
+}
+
+$rs = $dbo->query("SHOW DATABASES");
+while ($h = $rs->fetch(PDO::FETCH_NUM)) {
+    echo $r[0] . "<br>";
+}
