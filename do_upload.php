55e787c764318bc1798e390ca489644cdcc9b9a0
Ryan McCullagh <ryan@amezmo.com>
2020-12-30 13:16:10 -0600

    aa

--- do_upload.php
+++ do_upload.php
@@ -3,15 +3,16 @@ $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
-    $uploadOk = 1;
-  } else {
-    echo "File is not an image.";
-    $uploadOk = 0;
-  }
+$file_tmp = $_FILES['fileToUpload']['tmp_name'];
+
+if (! is_dir('uploads')) {
+    mkdir('uploads');
 }
+
+move_uploaded_file($file_tmp,"uploads/".$file_name);
+
+echo "File path is uploads/{$file_name}";
+
+echo "File is an image - " . $check["mime"] . ".";
+
 ?>
