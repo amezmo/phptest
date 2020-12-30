d735c63dadb93e103fcf77b76e95ea3ce28cbac0
Ryan McCullagh <ryan@amezmo.com>
2020-12-30 13:20:30 -0600

    a

--- do_upload.php
+++ do_upload.php
@@ -1,18 +1,15 @@
 <?php
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-$file_tmp = $_FILES['fileToUpload']['tmp_name'];
 
 if (! is_dir('uploads')) {
     mkdir('uploads');
 }
 
-move_uploaded_file($file_tmp,"uploads/".$file_name);
+move_uploaded_file($_FILES['fileToUpload']['tmp_name'], "uploads/".$target_file);
 
-echo "File path is uploads/{$file_name}";
+var_dump($_FILES);
 
-echo "File is an image - " . $check["mime"] . ".";
+echo "File path is uploads/{$target_file}";
 
 ?>
