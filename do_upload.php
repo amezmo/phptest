82d122b6e32aaf4c03c1330f592461e5385b0723
Ryan McCullagh <ryan@amezmo.com>
2020-12-30 13:23:18 -0600

    dd

--- do_upload.php
+++ do_upload.php
@@ -6,7 +6,7 @@ if (! is_dir('uploads')) {
     mkdir('uploads');
 }
 
-move_uploaded_file($_FILES['fileToUpload']['tmp_name'], "uploads/".$fileName);
+move_uploaded_file($_FILES['fileToUpload']['tmp_name'], "./uploads/".$fileName);
 
 var_dump($_FILES);
 
