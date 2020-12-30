04e51970d8544f6d20c7ae7169d9e8b6539c1dc7
Ryan McCullagh <ryan@amezmo.com>
2020-12-30 13:21:59 -0600

    aa

--- do_upload.php
+++ do_upload.php
@@ -1,15 +1,15 @@
 <?php
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+
+$fileName = $_FILES["fileToUpload"]["name"];
 
 if (! is_dir('uploads')) {
     mkdir('uploads');
 }
 
-move_uploaded_file($_FILES['fileToUpload']['tmp_name'], "uploads/".$target_file);
+move_uploaded_file($_FILES['fileToUpload']['tmp_name'], "uploads/".$fileName);
 
 var_dump($_FILES);
 
-echo "File path is uploads/{$target_file}";
+echo "File path is uploads/{$fileName}";
 
 ?>
